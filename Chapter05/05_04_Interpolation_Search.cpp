e690690c29cf0b0638b25632752d0b685d9a394b
lkm543 <lkm543@gmail.com>
2023-05-08 01:15:31 +0800

    Update 05_04_Interpolation_Search.cpp

--- Chapter05/05_04_Interpolation_Search.cpp
+++ Chapter05/05_04_Interpolation_Search.cpp
@@ -8,20 +8,24 @@ int Interpolation_Search(int data[], int lower, int upper, int target){
         return -1;
     int upper_data = data[upper];
     int lower_data = data[lower];
-    // 透過公式算出目標最可能在的索引值 index
-    int index = lower + (target - lower_data) * (upper - lower) / (upper_data - lower_data);
-    // 與二分搜尋法一樣分成三種情況處理
-    // data[index] 正好就是目標
-    if(data[index] == target)
-        return index;
-    // data[index] 比目標的值大
-    // 尋找左側區間
-    else if(data[index] > target)
-        return Interpolation_Search(data, lower, index - 1, target);
-    // data[index] 比目標的值小
-    // 尋找右側區間
-    else if(data[index] < target)
-        return Interpolation_Search(data, index + 1, upper, target);
+
+    if(upper >= lower && target >= lower_data && target <= upper_data){
+        // 透過公式算出目標最可能在的索引值 index
+        int index = lower + (target - lower_data) * (upper - lower) / (upper_data - lower_data);
+        // 與二分搜尋法一樣分成三種情況處理
+        // data[index] 正好就是目標
+        if(data[index] == target)
+            return index;
+        // data[index] 比目標的值大
+        // 尋找左側區間
+        else if(data[index] > target)
+            return Interpolation_Search(data, lower, index - 1, target);
+        // data[index] 比目標的值小
+        // 尋找右側區間
+        else if(data[index] < target)
+            return Interpolation_Search(data, index + 1, upper, target);
+    }
+    return -1;
 }
 
 int main()
